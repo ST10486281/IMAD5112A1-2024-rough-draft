ff450032ddbf0c8f8031de2dde3a33db8ba62b06
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-02 09:49:06 +0000

    The start of my project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD5112A1_2024_rough_draft"
+include(":app")
+ 
